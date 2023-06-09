77d0bc6b7fcc4f719c41450171d092ff223270a2
evert789 <eaam789@gmail.com>
2023-06-09 16:05:57 -0500

    Ejercicios Crear, Migrar, Actualizar, eliminar

---
+++ database/migrations/2023_06_09_202913_create_autos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('autos', function (Blueprint $table) {
+            $table->increments("id");
+            $table->string("marca");
+            $table->string("modelo"); 
+            $table->string("kilometraje");
+            $table->string("trasmision");
+            $table->string("puertas");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('autos');
+    }
+};
